5dce7f1c128474cb5a723723f3d87e8ea6c54644
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-06 10:56:11 +0500

    Models added and setup startserver function

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
-import express from "express"
-import { ENV } from "./config/env.js"
-import { connectDB } from "./config/db.js"
+import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
-const app = express()
+const app = express();
 
-app.get("/",(req,res)=>res.send("Hello from server"))
+app.get("/", (req, res) => res.send("Hello from server"));
 
-connectDB()
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log("Server running on:", ENV.PORT);
+    });
+  } catch (error) {
+    console.log("Error starting the server", error);
+    process.exit(1);
+  }
+};
 
-app.listen(ENV.PORT,()=>{
-  console.log('Server running on:',ENV.PORT)
-})
+startServer();
