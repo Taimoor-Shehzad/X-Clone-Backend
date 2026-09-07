f6f9fff37a2d492b6453c88218a75b4b4116a7c5
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-07 10:49:16 +0500

    Established all the user routes

---
+++ src/routes/user.route.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  followUser,
+  getCurrentUser,
+  getUserProfile,
+  syncUser,
+  updateProfile,
+} from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+//public
+router.get("/profile/:username", getUserProfile);
+
+//authenticated
+router.put("/profile", protectRoute, updateProfile);
+router.post("/sync", protectRoute, syncUser);
+router.get("/me", protectRoute, getCurrentUser);
+router.post("/follow/:targetUserId", protectRoute, followUser);
+
+export default router;
