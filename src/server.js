19727828d3a5ca23a0e241cb5855dc2b908692e8
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-15 17:44:10 +0500

    commenting out arcjet middleware

--- src/server.js
+++ src/server.js
@@ -70,7 +70,7 @@ app.set("trust proxy", true);
 app.use(express.json());
 app.use(clerkMiddleware());
 app.use(cors());
-app.use(arcjetMiddleware());
+// app.use(arcjetMiddleware());
 
 const startServer = async () => {
   try {
