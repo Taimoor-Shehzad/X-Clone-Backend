4998117c15aae766604e80815152d8672535a79c
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-15 17:54:52 +0500

    comitted code cleanup

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,3 @@
-// import mongoose from "mongoose";
-// import { ENV } from "./env.js";
-
-// let isConnected = false;
-
-// export const connectDB = async () => {
-//   if (isConnected && mongoose.connection.readyState === 1) {
-//     return;
-//   }
-
-//   try {
-//     const db = await mongoose.connect(ENV.MONGO_URI);
-//     isConnected = db.connections[0].readyState === 1;
-//     console.log("DB connected successfully ✅");
-//   } catch (error) {
-//     console.error("Error connecting to MONGODB:", error.message);
-//     throw error; // Throw error to be caught by request middleware instead of crashing the process
-//   }
-// };
-
 import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
