68e790ba1d41ea4ff8f936374d867d6c212b147b
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-15 15:09:45 +0500

    server.js fixings

--- src/server.js
+++ src/server.js
@@ -62,28 +62,15 @@ import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
-import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
+// import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 app.set("trust proxy", true);
 
 app.use(express.json());
 app.use(clerkMiddleware());
-
 app.use(cors());
 // app.use(arcjetMiddleware);
-
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/notifications", notificationRoutes);
-
-app.use((err, req, res, next) => {
-  console.log("Unhadled Error", err);
-  res.status(500).json({ error: err.message || "Internal server error" });
-});
-
-app.get("/", (req, res) => res.send("Hello from server"));
 
 const startServer = async () => {
   try {
@@ -102,6 +89,18 @@ const startServer = async () => {
 };
 
 startServer();
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+app.use((err, req, res, next) => {
+  console.log("Unhadled Error", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
+
+app.get("/", (req, res) => res.send("Hello from server"));
 
 // for vercel
 export default app;
