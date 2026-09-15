bb4709b9bf8a3a59a47c7d51dd1803a45a529918
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-15 18:09:51 +0500

    Fixed the userSync route problem succesfully

--- src/server.js
+++ src/server.js
@@ -7,28 +7,15 @@ import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
-import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
+// import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 app.set("trust proxy", true);
 
 app.use(express.json());
 app.use(clerkMiddleware());
-
 app.use(cors());
-app.use(arcjetMiddleware);
-
-app.use("/api/user", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/notifications", notificationRoutes);
-
-app.use((err, req, res, next) => {
-  console.log("Unhadled Error", err);
-  res.status(500).json({ error: err.message || "Internal server error" });
-});
-
-app.get("/", (req, res) => res.send("Hello from server"));
+// app.use(arcjetMiddleware());klk
 
 const startServer = async () => {
   try {
@@ -47,6 +34,18 @@ const startServer = async () => {
 };
 
 startServer();
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+app.use((err, req, res, next) => {
+  console.log("Unhadled Error", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
+
+app.get("/", (req, res) => res.send("Hello from server"));
 
 // for vercel
 export default app;
