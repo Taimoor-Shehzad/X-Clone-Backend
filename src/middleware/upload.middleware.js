22eccb9302667aef4093031b8b6bbf9e5094568f
Taimoor Shehzad <taimoorahmedshehzad@gmail.com>
2026-09-20 15:06:53 +0500

    upload middleware typo fix

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -3,7 +3,7 @@ import multer from "multer";
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimitype.startsWith("image/")) {
+  if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
     cb(new Error("Only image files are allowed"), false);
